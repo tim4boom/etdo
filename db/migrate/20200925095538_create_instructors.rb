975975d3707cac1aad9a3d68d3e11d590738d8c9
Tim Vierboom <tim-vierboom@gmx.de>
2020-09-25 11:56:41 +0200

    created instructor model

---
+++ db/migrate/20200925095538_create_instructors.rb
@@ -0,0 +1,11 @@
+class CreateInstructors < ActiveRecord::Migration[6.0]
+  def change
+    create_table :instructors do |t|
+      t.string :name
+      t.string :location
+      t.string :contact
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_25_090711) do
+ActiveRecord::Schema.define(version: 2020_09_25_095538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "courses", force: :cascade do |t|
-    t.string "name"
     t.text "description"
     t.text "theory"
     t.text "practice"
     t.text "requirements"
     t.text "length"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+  end
+
+  create_table "instructors", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "contact"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
