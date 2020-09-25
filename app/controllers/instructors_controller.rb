bda4f0d3c1cce9d7b97dc18ef1af98c8877870ac
Tim Vierboom <tim-vierboom@gmx.de>
2020-09-25 13:57:03 +0200

    added first instructor

---
+++ app/controllers/instructors_controller.rb
@@ -0,0 +1,11 @@
+class InstructorsController < ApplicationController
+  def index
+    @instructors = Instructor.all
+  end
+
+  private
+
+  def course_params
+    params.require(:instructor).permit(:name, :location, :contact)
+  end
+end
