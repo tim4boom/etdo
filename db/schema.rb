a6c8fc10c3e726c9680da812101b377c524a20b5
Tim Vierboom <tim-vierboom@gmx.de>
2020-09-25 10:48:53 +0200

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_25_082922) do
+ActiveRecord::Schema.define(version: 2020_09_25_084459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "courses", force: :cascade do |t|
+    t.text "description"
+    t.text "theory"
+    t.text "practice"
+    t.text "requirements"
+    t.text "length"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
