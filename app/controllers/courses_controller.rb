e11b38eb9a4fc7c52ee6f4322e73a916fca545f4
Tim Vierboom <tim-vierboom@gmx.de>
2020-09-25 10:56:11 +0200

    added index and show action to course controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,19 @@
 class CoursesController < ApplicationController
+  def index
+    @courses = Course.all
+  end
+
+  def show
+    find_course
+  end
+
+  private
+
+  def find_course
+    @course = Course.find(params[:id])
+  end
+
+  def course_params
+    params.require(:course).permit(:description, :theory, :practice, :requirements, :length)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
   devise_for :users
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :courses, only [ :index, :show ]
 end
