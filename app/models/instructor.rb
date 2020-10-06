925bc20a76c6212148d055de2b7dc3f870e8317b
Tim Vierboom <tim-vierboom@gmx.de>
2020-10-06 15:37:12 +0200

    added styling to instructor form

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,5 @@
 class Instructor < ApplicationRecord
   has_one_attached :avatar
+  has_rich_text :location
+  has_rich_text :contact
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :courses, only: [ :index, :show, :new, :create, :edit, :update, :destroy ]
-  resources :instructors, only: :index
+  resources :instructors, only: [ :index, :new, :create, :edit, :update, :destroy ]
 
   get '/about', to: 'pages#about'
   get '/disclaimer', to: 'pages#disclaimer'
